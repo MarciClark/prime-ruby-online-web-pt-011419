6f5c6ea3455e8166a59e0c9c1b1751d7c96bfbb6
Marci Clark <marcig612@gmail.com>
2019-01-26 04:17:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,8 @@
 def prime?(num)
-  if num >= 2
+  if num <= 2
+    false 
+    elsif n == 2 
+    true
     (2..num -1).all? do |x|
       num % x != 0 
     end
@@ -9,3 +12,12 @@ def prime?(num)
 end
 
 
+def prime? (num)
+    if num <= 1
+        false
+    elsif num == 2
+        true
+    else 
+        (2..n/2).none? { |i| num % i == 0}
+    end
+end
